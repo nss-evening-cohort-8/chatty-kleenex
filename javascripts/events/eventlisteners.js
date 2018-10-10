298e7e81d2759c8dc623498ebe62c34db95605ae
Colin White <clwhite814@gmail.com>
2018-10-09 19:48:19 -0500

    wip

--- javascripts/events/eventlisteners.js
+++ javascripts/events/eventlisteners.js
@@ -1,1 +1,8 @@
 console.log('hi');
+
+const darkTheme = () => {
+    const darkCheckBox = document.getElementById('darkTheme');
+    darkCheckBox.addEventListener('checked', (e) => {
+        console.log('its working');
+    });
+};
